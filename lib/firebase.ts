9102179ea6d3373529ea99cca32b404066340791
Alayande Esther <alayandeesther469@gmail.com>
2026-08-20 21:15:40 +0100

    added keys to env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDY3Oy2pN2C2KWP1b8m3Z0c4xeWpfSxG7w",
-  authDomain: "foodies-2c3c2.firebaseapp.com",
-  projectId: "foodies-2c3c2",
-  storageBucket: "foodies-2c3c2.firebasestorage.app",
-  messagingSenderId: "778301589732",
-  appId: "1:778301589732:web:2d6199b3d0267fc52644f1",
-  measurementId: "G-B25VQL29N6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
